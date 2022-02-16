77e368b7deefb1218841b25c3dc013ba83620630
Laura Villarreal <>
2022-02-16 07:45:33 -0600

    Walking and gravity script created; basic jump added but needs work; edited followPlayer.cs and added a camera that can be moved by the mouse

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,21 +5,37 @@ using UnityEngine.Assertions;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public GameObject followObject;
+    /*public GameObject followObject;
     
     Transform cameraPos;
+    Vector3 relativePos;*/
+
+    public Transform followObject;
+    public float smoothing = 5f;
+
     Vector3 relativePos;
+
     // Start is called before the first frame update
     void Start()
     {
-        Assert.IsNotNull(followObject);
-        cameraPos = GetComponent<Transform>();
-        relativePos = cameraPos.position - followObject.transform.position;
+        /* Assert.IsNotNull(followObject);
+         cameraPos = GetComponent<Transform>();
+         relativePos = cameraPos.position - followObject.transform.position;
+        */
+        // calculate initial offset
+        relativePos = transform.position - followObject.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        cameraPos.position = followObject.transform.position + relativePos;
+        //cameraPos.position = followObject.transform.position + relativePos;
+
+        // create position camera is aiming for
+        Vector3 targetCameraPos = followObject.position + relativePos;
+
+        // smooth movement between camera's current position and game object
+        transform.position = Vector3.Lerp(transform.position, targetCameraPos, smoothing * Time.deltaTime);
+
     }
 }
---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 6f;
+    private float fallSpeed = 15f;
+    public float jumpHeight = 3f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    // variables for gravity and jumping
+    private float gravity = 9.8f;
+    private float verticalVelocity = 0;
+    Vector3 velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get the value of the virtual axis. Values will either be
+        // -1, 0, or 1 in movement, no in betweens
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical"); 
+        
+        // allows for movement in x,y, and z directions
+        // normalized so that we don't go faster when we go diagonal (hitting 2 keys)
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            // player rotation
+            // get the target angle of our movement in degrees
+            // add by cam.eulerAngles.y to make sure player points the right way relative to the camera
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+
+            // smooth the rotation before it occurs (transform.eulerAngles.y is our current angle)
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            // rotate on the y
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            // Allows player movement to adapt to wherever the camera is pointing
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            // deltaTime allows the movement to be independent of frame rate
+            // move the character
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+        // Gravity Code
+        // if its on the ground, vertical velocity is 0
+        if (controller.isGrounded)
+        {
+            verticalVelocity = 0f;
+        }
+        // if on the ground and space is pushed, verticalVelocity changes
+        if (controller.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            verticalVelocity = jumpHeight;
+        }
+        // to determine the player's y position if they fall or jump
+        verticalVelocity = verticalVelocity + direction.y - (gravity * Time.deltaTime);
+        velocity.y = verticalVelocity;
+
+        controller.Move(velocity * fallSpeed * Time.deltaTime);
+
+
+    }
+}
